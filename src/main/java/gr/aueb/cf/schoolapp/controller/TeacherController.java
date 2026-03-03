6d6d1078e91250342d304e1c3711dda5d33a4e28
evitsapara <evi.tsapi@gmail.com>
2026-03-03 21:31:22 +0200

    feat: add / remove features for teacher insert

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -2,11 +2,17 @@ package gr.aueb.cf.schoolapp.controller;
 
 import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+//import gr.aueb.cf.schoolapp.model.Teacher;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -19,6 +25,30 @@ public class TeacherController {
         model.addAttribute("teacherInsertDTO", TeacherInsertDTO.empty());
         // model.addAttribute("regionsReadOnlyDTO", regions());
         return "teacher-insert";
+    }
+
+    @PostMapping("/insert")
+    public String teacherInsert(@Valid @ModelAttribute("teacherInsertDTO") TeacherInsertDTO teacherInsertDTO,
+                                BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+//            model.addAttribute("regionsReadOnlyDTO", regions());
+        return "teacher-insert";
+        }
+
+        // save teacher to DB
+        TeacherReadOnlyDTO teacherReadOnlyDTO = new TeacherReadOnlyDTO("acfd-1234", "Athanasios", "Androutsos");
+//        model.addAttribute("teacherReadOnlyDTO", teacherReadOnlyDTO);
+
+        // PRG - Post-Redirect-Get
+        redirectAttributes.addFlashAttribute("teacherReadOnlyDTO", teacherReadOnlyDTO);
+        return "redirect:/teachers/success";
+
+    }
+
+    @GetMapping("/success")
+    public String teacherSuccess(Model model) {
+        return "teacher-success";
     }
 
     @ModelAttribute("regionsReadOnlyDTO")           // Executed before each request handler
