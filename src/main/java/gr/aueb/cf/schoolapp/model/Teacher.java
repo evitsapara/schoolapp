aa0bc2e5008898d11ed18f77c8265c511287bed4
evitsapara <evi.tsapi@gmail.com>
2026-03-04 19:34:03 +0200

    feat: enable JPA and define AbstractEntity and Teacher

--- src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
@@ -1,18 +1,36 @@
-//package gr.aueb.cf.schoolapp.model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Table;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Setter
-//@Table(name = "teachers")
-//public class Teacher extends AbstractEntity {
-//
-//}
+package gr.aueb.cf.schoolapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "teachers")
+public class Teacher extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private UUID uuid;
+
+    @Column(unique = true)
+    public String vat;
+
+    String firstname;
+    String lastname;
+
+    @PrePersist
+    public void initializeUUID() {
+        this.uuid = UUID.randomUUID();
+    }
+}
